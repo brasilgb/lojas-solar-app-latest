59df8fe115922d6e249ec6596ca63f7b68247315
Anderson Brasil <andersonbrasil72@gmail.com>
2026-03-24 10:50:36 -0300

    Push

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-import 'expo-router/entry';
-import { getApp } from '@react-native-firebase/app';
-import {
-  getMessaging,
-  setBackgroundMessageHandler,
-} from '@react-native-firebase/messaging';
 import notifee from '@notifee/react-native';
+import firebase, { getApp } from '@react-native-firebase/app';
+import {
+    getMessaging,
+    setBackgroundMessageHandler,
+} from '@react-native-firebase/messaging';
+import 'expo-router/entry';
 
 import {
-  displayNotification,
-  handleNotifeeBackgroundEvent,
-  parseRemoteMessage,
-  setupNotificationChannel,
+    displayNotification,
+    handleNotifeeBackgroundEvent,
+    parseRemoteMessage,
+    setupNotificationChannel,
 } from './src/lib/notifications';
+
+// Initialize Firebase if not already initialized
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyAoS4j1-tu5uofMCbOlYhV1Oj_vwt3Ej_4',
+    authDomain: 'applojassolar.firebaseapp.com',
+    projectId: 'applojassolar',
+    storageBucket: 'applojassolar.firebasestorage.app',
+    messagingSenderId: '639262521508',
+    appId: '1:639262521508:ios:fc6fa1841be87274318e31',
+    databaseURL: 'https://applojassolar-default-rtdb.firebaseio.com/',
+  });
+}
 
 const messagingInstance = getMessaging(getApp());
 
